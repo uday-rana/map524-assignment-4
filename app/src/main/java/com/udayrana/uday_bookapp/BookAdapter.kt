d07bd535ad1239e688b7be1f982928408a68a4e0
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-05 01:19:42 -0400

    Setup RecyclerView

---
+++ app/src/main/java/com/udayrana/uday_bookapp/BookAdapter.kt
@@ -0,0 +1,24 @@
+package com.udayrana.uday_bookapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.udayrana.uday_bookapp.databinding.BookItemBinding
+
+class BookAdapter(val bookList: MutableList<Book>, val clickListener: BookClickListener) :
+    RecyclerView.Adapter<BookAdapter.ViewHolder>() {
+    inner class ViewHolder(val binding: BookItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = BookItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return bookList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val book = bookList
+    }
+}
---
+++ app/src/main/java/com/udayrana/uday_bookapp/BookClickListener.kt
@@ -0,0 +1,5 @@
+package com.udayrana.uday_bookapp
+
+interface BookClickListener {
+    fun displayBook(id: Int)
+}
